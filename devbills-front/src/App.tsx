356196fd4f15bce41e21ba351a4eb012a0d905b1
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-08-16 02:10:46 -0300

    Components App e Header + Logo

--- devbills-front/src/App.tsx
+++ devbills-front/src/App.tsx
@@ -1,10 +1,11 @@
+import { Home } from "./screens/home";
 import GlobalStyles from "./styles/global";
 
 export function App() {
   return (
     <>
       <GlobalStyles />
-      <h1>DevBills</h1>
+      <Home />
     </>
   );
 }
---
+++ devbills-front/src/screens/home/index.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "../components/logo";
+import { Header } from "./styles";
+
+export function Home() {
+  return (
+    <Header>
+      <Logo />
+    </Header>
+  );
+}
